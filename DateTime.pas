d377ad61af44eaa336803a5e1461e9af95664aca
Danil <danilgordienko212@gmail.com>
2022-10-06 16:54:39 +0300

    add new fumction

---
+++ DateTime.pas
@@ -0,0 +1,13 @@
+﻿begin
+  var god := ReadInteger();
+  if ((god mod 4 = 0) and ((god mod 100 <> 0) or (god mod 400 = 0))) then
+  begin
+    Println('Год выскосный');
+    Print('366');
+  end
+  else
+  begin
+    Println('Год невыскосный');
+    Print('365');
+  end;
+end.
