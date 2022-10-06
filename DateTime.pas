7564d038e0c4d461e7f15c3072cd5895673059b2
Danil <danilgordienko212@gmail.com>
2022-10-06 16:51:14 +0300

    add file.pas

---
+++ DateTime.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var god := ReadInteger();
+  if ((god mod 4 = 0) and ((god mod 100 <> 0) or (god mod 400 = 0))) then
+    Print('Год выскосный')
+  else
+    Print('Год невыскосный');
+end.
